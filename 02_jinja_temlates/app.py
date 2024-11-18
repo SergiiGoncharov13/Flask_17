2b6b76ef6b2c0d1193c698e20aeeec3e75b96b0a
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2024-11-18 19:32:43 +0200

    add_folder

---
+++ 02_jinja_temlates/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+max_score = 100
+test_name = 'Python Challenge'
+students = [
+    {'name':'Vlad', 'score': 100},
+    {'name':'Svitoslav', 'score': 99},
+    {'name':'Ustin', 'score': 100},
+    {'name':'Victor', 'score': 79},
+    {'name':'Yaroslav', 'score': 73},
+]
+
+@app.route('/')
+def index():
+    return render_template('base.html', title='jinja_test_Flask')
+
+
+@app.route('/result')
+def result():
+    context = {
+        'title': 'Results',
+        'students': students,
+        'test_name': test_name,
+        'max_score': max_score
+    }
+    return render_template('results.html', **context)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
