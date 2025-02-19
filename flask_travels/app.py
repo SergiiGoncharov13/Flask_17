3fb8ea72bc82e3c11def4dc535299aa801fe8007
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2025-02-19 19:48:09 +0200

    flask_travels

---
+++ flask_travels/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+import data
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        departures=data.departures,
+        title=data.title,
+        subtitle=data.subtitle,
+        description=data.description,
+        tours=data.tours
+        )
+
+
+@app.route('/departures/departure')
+def departure(departure):
+    tours = dict(filter(lambda tour: tour[1]['departure'] == departure, data.tours.items()))
+    if tours:   
+        return render_template(
+            'departure.html',
+            departure=departure,
+            title=data.title,
+            departures=data.departures,
+            tours=tours
+            )
+    abort(404)
+
+
+@app.route('/departures/')
+def departure_zero():
+    return render_template('index.html')
+
+
+
+@app.route('/tours/<int:id>')
+def tour(id):
+    return render_template(
+        'tour.html',
+        tour=data.tours[id],
+        title=data.title,
+        departures=data.departures
+        )
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
