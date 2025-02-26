94f2a3d5cec4142ac8413a6771947a2127801730
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2025-02-26 21:36:13 +0200

    add Order page

--- flask_shop/app.py
+++ flask_shop/app.py
@@ -17,6 +17,25 @@ def products():
     return render_template('products.html', products=all_produdts)
         
 
+@app.route('/order/<int:product_id>', methods=['POST', 'GET'])
+def order(product_id):
+    with Session() as session:
+        product = session.query(Product).get(product_id)
+        if not product:
+            return 'Boots not found', 404
+
+        if request.method == 'POST':
+            phone = request.form['phone']
+            email = request.form['email']
+
+            new_order =Orders(phone=phone, email=email, product_id=product_id)
+            session.add(new_order)
+            session.commit()
+
+            return redirect(url_for('index'))
+        
+        return render_template('order.html', product=product)
+
 
 
 if __name__ == '__main__':
